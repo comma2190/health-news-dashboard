e6f9942f53329f25dd3595ce86237b190b967b13
comma2190 <comma2190@gmail.com>
2026-05-30 03:49:44 +0900

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,3 +1,23 @@
+import { NextResponse } from 'next/server';
+
 export function middleware(req) {
-  return;
+
+  const auth = req.cookies.get('auth');
+
+  const path = req.nextUrl.pathname;
+
+  if(
+    path === '/login.html' ||
+    path.startsWith('/api/login')
+  ){
+    return NextResponse.next();
+  }
+
+  if(auth?.value === 'ok'){
+    return NextResponse.next();
+  }
+
+  return NextResponse.redirect(
+    new URL('/login.html', req.url)
+  );
 }
