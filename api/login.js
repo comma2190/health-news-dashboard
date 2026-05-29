e7a7c8e52d0a405596154b62e35e0b7bd8bcf46e
comma2190 <comma2190@gmail.com>
2026-05-30 03:49:26 +0900

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,5 +1,19 @@
 export default function handler(req, res) {
-  return res.status(200).json({
-    message: 'login api'
-  });
+
+  if(req.method !== 'POST'){
+    return res.status(405).end();
+  }
+
+  const { password } = req.body;
+
+  if(password !== process.env.PAGE_PASSWORD){
+    return res.status(401).json({ ok:false });
+  }
+
+  res.setHeader(
+    'Set-Cookie',
+    'auth=ok; Path=/; HttpOnly; SameSite=Lax; Max-Age=86400'
+  );
+
+  return res.status(200).json({ ok:true });
 }
